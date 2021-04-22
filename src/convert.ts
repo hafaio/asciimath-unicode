367f60806108fe94a2e1b7c143afee7b35587eab
Erik Brinkman <erik.brinkman@gmail.com>
2021-04-22 16:13:49 -0400

    allow convert to work in textarea or input elements

--- src/convert.ts
+++ src/convert.ts
@@ -2,7 +2,7 @@ import parse from "../lib/parse";
 import render from "../lib/render";
 import { isOptions, defaultOptions } from "../lib/options";
 
-// replace selection
+// replace selection on page
 const selection = getSelection();
 if (selection && selection.rangeCount) {
   // valid selection
@@ -14,6 +14,25 @@ if (selection && selection.rangeCount) {
       const replacement = render(parse(selection.toString()), options);
       range.deleteContents();
       range.insertNode(document.createTextNode(replacement));
+    });
+  }
+}
+
+// replace selection in active element (input or text area)
+const active = document.activeElement;
+if (
+  active instanceof HTMLInputElement ||
+  active instanceof HTMLTextAreaElement
+) {
+  const start = active.selectionStart;
+  const end = active.selectionEnd;
+  if (start !== null && end !== null && start < end) {
+    const value = active.value;
+    chrome.storage.sync.get(defaultOptions, (opts) => {
+      const options = isOptions(opts) ? opts : defaultOptions;
+      const replacement = render(parse(value.slice(start, end)), options);
+      active.value = value.slice(0, start) + replacement + value.slice(end);
+      active.setSelectionRange(start, start + replacement.length);
     });
   }
 }
