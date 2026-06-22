b86715c378c1fe62a5bb2d4a5f2efcb0251e6b4f
Erik Brinkman <erik.brinkman@gmail.com>
2026-06-22 02:01:57 -0400

    chore: clean up

---
+++ src/options-main.tsx
@@ -0,0 +1,5 @@
+import { createRoot } from "react-dom/client";
+import OptionsPage from "../components/options-page";
+
+const container = document.getElementById("root")!;
+createRoot(container).render(<OptionsPage />);
